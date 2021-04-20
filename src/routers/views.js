20be7a47600311770758ed740024441ec3ca0091
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2021-04-20 16:38:55 +0530

    Added a route for the patientAddForm

--- src/routers/views.js
+++ src/routers/views.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const Patients = require('../models/patients')
 
 router.get('',async (req, res) => {
     
@@ -28,4 +29,25 @@ router.get('/individualModule',async (req, res) => {
     }
     
 })
+
+router.post('/addPatientFormData',async(req,res)=>{
+    try {
+        console.log(req.body)
+
+        // const patient = new Patients({
+        // ...req.body,
+        // })
+    
+    // const storeData = await patient.save()
+    // const patientGameSettings = new gameSettings({
+    //     patientId:storeData._id
+    // })
+    // await patientGameSettings.save()
+    res.status(200).send('Patient Added Successfully')  
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
+
 module.exports = router
