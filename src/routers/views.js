c50d1570745d21800e9a91119f7aaad1e4eb3dd7
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2021-04-24 10:49:29 +0200

    Configured to save the data in the database

--- src/routers/views.js
+++ src/routers/views.js
@@ -1,53 +1,48 @@
 const express = require('express')
 const router = new express.Router()
-const Patients = require('../models/patients')
+const Patient = require('../models/patients')
 
-router.get('',async (req, res) => {
-    
+router.get('', async (req, res) => {
+
     try {
-        res.render('gameHome')  
+        res.render('gameHome')
     } catch (e) {
         console.log(e)
     }
-    
+
 })
-router.get('/subModuleHolder',async (req, res) => {
-    
+router.get('/subModuleHolder', async (req, res) => {
+
     try {
-        res.render('subModuleHolder')  
+        res.render('subModuleHolder')
     } catch (e) {
         console.log(e)
     }
-    
+
 })
-router.get('/individualModule',async (req, res) => {
-    
+router.get('/individualModule', async (req, res) => {
+
     try {
-        res.render('individualModuleHome')  
+        res.render('individualModuleHome')
     } catch (e) {
         console.log(e)
     }
-    
+
 })
 
-router.post('/addPatientFormData',async(req,res)=>{
+router.post('/addPatientFormData', async (req, res) => {
     try {
-        console.log(req.body)
-
-        // const patient = new Patients({
-        // ...req.body,
-        // })
-    
-    // const storeData = await patient.save()
-    // const patientGameSettings = new gameSettings({
-    //     patientId:storeData._id
-    // })
-    // await patientGameSettings.save()
-    res.status(200).send('Patient Added Successfully')  
-    } catch (error) {
+        // console.log(req.body)
+        const patient = new Patient({
+            ...req.body,
+        })
+        await patient.save()
+        res.status(200).send('Patient Added Successfully')
+    }
+    catch (error) {
         console.log(error)
     }
-    
+
 })
 
 module.exports = router
