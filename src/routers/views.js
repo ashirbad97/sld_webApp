e1e10651c6330ff278e25b0a0aaa8612d74ddccd
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2021-04-27 13:14:49 +0530

    Changed the routes to allow the authentication of the patient to login

--- src/routers/views.js
+++ src/routers/views.js
@@ -3,33 +3,20 @@ const router = new express.Router()
 const Patient = require('../models/patients')
 const auth = require('../middleware/authentication')
 
-router.post('/login', async (req, res) => {
-    try {
-        res.status(200)
-        const patient = await Patient.authenticateuser(req.body.username,req.body.password)
-        console.log(patient)
-        const token = await patient.generateAuthToken()
-        // if(!token){
-        //     throw error()
-        // }
-        // console.log("Hello"+patient)
-        // const doctorInfo = await getDoctorInfo(doctor._id)
-        // res.cookie('auth_token', token).render('index',{doctorInfo})
-        // res.cookie('auth_token',token).render('gamehome')
-    } catch (e) {
-        console.log(e)
-        // res.render('login',{e})
-    }
-})
+
 
 router.get('/',auth, async (req, res) => {
-
     try {
-        res.render('gameHome')
+        patient = req.patient
+        res.render('gameHome',{patient})
     } catch (e) {
         console.log(e)
     }
 
+})
+
+router.get('/login',(req, res) => {
+    res.render('login')
 })
 
 router.get('/subModuleHolder', async (req, res) => {
@@ -67,8 +54,20 @@ router.post('/addPatientFormData', async (req, res) => {
     }
 })
 
-router.get('/login',(req, res) => {
-    res.render('login')
+router.post('/login', async (req, res) => {
+    try {
+        res.status(200)
+        var patient = await Patient.authenticateuser(req.body.username,req.body.password)
+        const token = await patient.generateAuthToken()
+        patient = await patient.trimPatientData()
+        if(!token){
+            throw error()
+        }
+        res.cookie('auth_token',token).render('gamehome',{patient})
+    } catch (e) {
+        console.log(e)
+        res.render('login',{e})
+    }
 })
 
 module.exports = router
