5ff2f4d16096c7460a91caf2d7800987dc3c51df
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2021-06-03 22:37:11 +0200

    Duplicated commit for #10 as previous one had issues

--- src/utility/tempdb.js
+++ src/utility/tempdb.js
@@ -1,4 +1,4 @@
-// const mongoose = require('../db/mongoose')
+const mongoose = require('../db/mongoose')
 const GameLevel = require('../models/gameLevel')
 const Patient = require('../models/patients')
 const SubModule = require('../models/subModules')
@@ -8,12 +8,12 @@ const xlsx = require('xlsx')
 const fs = require('fs')
 const path = require('path')
 
-addGameLevel = async(i)=>{
+addGameLevel = async (i) => {
     try {
-        moduleName = 'Module_'+i
+        moduleName = 'Module_' + i
         const gameLevel = new GameLevel({
-            levelId:i,
-            levelName:moduleName
+            levelId: i,
+            levelName: moduleName
         })
         await gameLevel.save()
     } catch (error) {
@@ -21,7 +21,7 @@ addGameLevel = async(i)=>{
     }
 }
 // addGameLevel()
-showAllPatients = async()=>{
+showAllPatients = async () => {
     try {
         const patients = await Patient.find()
         console.log(patients)
@@ -41,17 +41,16 @@ entrySubModules = async () => {
         // subModuleName = ['at','ab','ag','an','ad','ap','ay','as','am','ax','en','et','ed','eg','ea','ee','ib','ig','it','id','in','ip','ix','ox','od','on','ob','og','op','ub','ut','un','ug','us','up','ack','ass','ang','ame','ave','and','age','ake','ane','amp','ank','ent','ead','eed','ing','iss','ick','ive','ipe','ite','ink','ice','ide','ope','ose','ock','all','ill','ike','oke','uck']
         // subModuleName = ['1 Letter Words','2 Letter Words','3 Letter Words','4 Letter Words','5 Letter Words','5+ Letter Words']
         // subModuleName = ['pl','wh','tr','th','sw','st','sp','sn','sl','sh','pr','gr','fr','fl','dr','cr','ch','bl','ph','dis','im','in','mis','pre','re','un']
-        subModuleName = ['nk','lk','nt','mp','ft','lt','lf','ld','st','rst','ng','ck','able','ful','less','ly','ment','ness']
+        subModuleName = ['nk', 'lk', 'nt', 'mp', 'ft', 'lt', 'lf', 'ld', 'st', 'rst', 'ng', 'ck', 'able', 'ful', 'less', 'ly', 'ment', 'ness']
         subModuleId = 8
         level = 'l8_'
-        for(i=0;i<subModuleName.length;i++)
-        {
-            value = level+(i+1)
+        for (i = 0; i < subModuleName.length; i++) {
+            value = level + (i + 1)
             const gameLevel = await GameLevel.findOne({ 'levelId': subModuleId })
             const subModule = new SubModule({
-                mainModule : gameLevel._id,
-                subModuleId : value,
-                subModuleName : subModuleName[i]
+                mainModule: gameLevel._id,
+                subModuleId: value,
+                subModuleName: subModuleName[i]
             })
             await subModule.save()
         }
@@ -61,10 +60,10 @@ entrySubModules = async () => {
     }
 }
 
-entryWords = async()=>{
+entryWords = async () => {
     try {
         subModuleId = 'l1_9'
-        const subModule = await SubModule.findOne({subModuleId})
+        const subModule = await SubModule.findOne({ subModuleId })
         // wordList = ['City','House','Snake','Glass','Palace']
         // wordList = ['Add','Apple','Snap','Fan','Animal']
         // wordList = ['Ten','Teddy','Bat','Soft','Letter']
@@ -75,10 +74,10 @@ entryWords = async()=>{
         // wordList = ['Egg','Head','Said','Elephant','Bread']
         // wordList = ['Hat','Hill','Hello','Hippo','Happy']
         wordList
-        for(i=0;i<wordList.length;i++){
+        for (i = 0; i < wordList.length; i++) {
             const word = new Word({
-                parentSubModule:subModule._id,
-                word:wordList[i]
+                parentSubModule: subModule._id,
+                word: wordList[i]
             })
             console.log(wordList[i])
             await word.save()
@@ -100,17 +99,17 @@ entryWords = async()=>{
 /////////////////////////////////////////////////////////////////////////
 subModuleId = "l8"
 
-renameSubModuleFileName = (subModuleId)=>{
+renameSubModuleFileName = (subModuleId) => {
     audioDir = "../../public/assets/audio"
-    audioPath = path.join(__dirname,audioDir)
-    subModulePath = path.join(audioPath,subModuleId)
+    audioPath = path.join(__dirname, audioDir)
+    subModulePath = path.join(audioPath, subModuleId)
 
-    fs.readdirSync(subModulePath).forEach(oldFileName=>{
-        newFileName = oldFileName.split("-")[1]+oldFileName.split("-")[2]
-        oldFilePath = path.join(subModulePath,oldFileName)
-        newFilePath = path.join(subModulePath,newFileName)
+    fs.readdirSync(subModulePath).forEach(oldFileName => {
+        newFileName = oldFileName.split("-")[1] + oldFileName.split("-")[2]
+        oldFilePath = path.join(subModulePath, oldFileName)
+        newFilePath = path.join(subModulePath, newFileName)
 
-        fs.rename(oldFilePath,newFilePath,()=>{
+        fs.rename(oldFilePath, newFilePath, () => {
             console.log("FileRenamed")
         })
     })
@@ -118,4 +117,14 @@ renameSubModuleFileName = (subModuleId)=>{
 }
 // renameSubModuleFileName(subModuleId)
 
+////////////////////////////////////////////////////////////////////////
+wordExtendedDetails = async () => {
+    try {
+        word = await Word.find({'word':'City'})
+        console.log(word)
+    } catch (error) {
+        console.log(error)
+    }
+}
+// wordExtendedDetails()
 ////////////////////////////////////////////////////////////////////////
